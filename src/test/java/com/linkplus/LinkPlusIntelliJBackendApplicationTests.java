a8f9dabb26d11a288daa9a569a8d0db0633c3272
Wonjeong Kim <38902021+NARUBROWN@users.noreply.github.com>
2023-08-14 02:15:07 +0900

    initial commit

---
+++ src/test/java/com/linkplus/LinkPlusIntelliJBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkPlusIntelliJBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
